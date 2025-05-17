090bd325ce6cda64ec692c44e1e5b4fd0cd6c753
P-joyce <joyceachen10@gmail.com>
2025-05-17 12:03:30 +0300

    exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+                numbers.Add(number);
+        } while (number != 0);
+
+        int sum = 0;
+        int max = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+                max = num;
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Max: {max}");
     }
 }
