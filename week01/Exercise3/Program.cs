0ccf41c03de8581fa12079f819b2cbad99b8e229
P-joyce <joyceachen10@gmail.com>
2025-05-17 11:59:56 +0300

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+                numbers.Add(number);
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+            sum += num;
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
