769c107b1fa3e5546bf978639180da12ca40a5d5
P-joyce <joyceachen10@gmail.com>
2025-05-19 09:02:41 +0300

    My week02 exercise

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,129 @@
 using System;
 
-class Program
+using System.Collections.Generic;
+using System.IO;
+
+namespace JournalProgram
 {
-    static void Main(string[] args)
+    public class Entry
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        public string Prompt { get; set; }
+        public string Response { get; set; }
+        public string Date { get; set; }
+
+        public Entry(string prompt, string response, string date)
+        {
+            Prompt = prompt;
+            Response = response;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date}\n{Prompt}\n{Response}\n";
+        }
+    }
+
+    public class Journal
+    {
+        public List<Entry> Entries { get; set; }
+        public string[] Prompts { get; set; }
+
+        public Journal()
+        {
+            Entries = new List<Entry>();
+            Prompts = new string[]
+            {
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?",
+                "If I had one thing I could do over today, what would it be?"
+            };
+        }
+
+        public void AddEntry()
+        {
+            Random random = new Random();
+            string prompt = Prompts[random.Next(Prompts.Length)];
+            Console.WriteLine(prompt);
+            string response = Console.ReadLine();
+            string date = DateTime.Now.ToShortDateString();
+            Entries.Add(new Entry(prompt, response, date));
+        }
+
+        public void DisplayEntries()
+        {
+            foreach (Entry entry in Entries)
+            {
+                Console.WriteLine(entry.ToString());
+            }
+        }
+
+        public void SaveToFile()
+        {
+            Console.Write("Enter filename: ");
+            string filename = Console.ReadLine();
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Entry entry in Entries)
+                {
+                    writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+                }
+            }
+        }
+
+        public void LoadFromFile()
+        {
+            Console.Write("Enter filename: ");
+            string filename = Console.ReadLine();
+            Entries.Clear();
+            string[] lines = File.ReadAllLines(filename);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('|');
+                Entries.Add(new Entry(parts[1], parts[2], parts[0]));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+            while (true)
+            {
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display journal");
+                Console.WriteLine("3. Save journal to file");
+                Console.WriteLine("4. Load journal from file");
+                Console.WriteLine("5. Quit");
+                Console.Write("Choose an option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        journal.AddEntry();
+                        break;
+                    case 2:
+                        journal.DisplayEntries();
+                        break;
+                    case 3:
+                        journal.SaveToFile();
+                        break;
+                    case 4:
+                        journal.LoadFromFile();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
     }
 }
+
+
